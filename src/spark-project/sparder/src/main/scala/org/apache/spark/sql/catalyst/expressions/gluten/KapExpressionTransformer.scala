c8317bcb3784341d219c33045a77372ae0a1519a
1319027852@qq.com <1319027852@qq.com>
2023-05-16 17:57:46 +0800

    Support bitmap function with gluten

--- src/spark-project/sparder/src/main/scala/org/apache/spark/sql/catalyst/expressions/gluten/KapExpressionTransformer.scala
+++ src/spark-project/sparder/src/main/scala/org/apache/spark/sql/catalyst/expressions/gluten/KapExpressionTransformer.scala
@@ -16,18 +16,24 @@
  */
 package org.apache.spark.sql.catalyst.expressions.gluten
 
+import io.glutenproject.backendsapi.clickhouse.CHBackendSettings
 import io.glutenproject.expression._
 import io.glutenproject.extension.ExpressionExtensionTrait
 import io.glutenproject.substrait.expression.{ExpressionBuilder, ExpressionNode}
 
 import org.apache.spark.internal.Logging
 import org.apache.spark.sql.catalyst.expressions._
-import io.glutenproject.backendsapi.clickhouse.CHBackendSettings
+import org.apache.spark.sql.catalyst.expressions.aggregate._
 import org.apache.spark.sql.internal.SQLConf
-import org.apache.spark.sql.types.StringType
+import org.apache.spark.sql.types.{BinaryType, DataType, LongType, StringType}
+import org.apache.spark.sql.udaf._
 import org.apache.spark.unsafe.types.UTF8String
 
+import com.google.common.collect.Lists
+import org.roaringbitmap.longlong.Roaring64NavigableMap
+
 import java.util.Locale
+import scala.collection.mutable.ListBuffer
 
 class FloorDateTimeExpressionTransformer(
   substraitExprName: String,
@@ -104,11 +110,38 @@ class FloorDateTimeExpressionTransformer(
   }
 }
 
+class KeBitmapFunctionTransformer(
+  substraitExprName: String,
+  child: ExpressionTransformer,
+  original: Expression) extends ExpressionTransformer with Logging {
+
+  override def doTransform(args: java.lang.Object): ExpressionNode = {
+    val childNode = child.doTransform(args)
+    val functionMap = args.asInstanceOf[java.util.HashMap[String, java.lang.Long]]
+    val functionId = ExpressionBuilder.newScalarFunction(
+      functionMap,
+      ConverterUtils.makeFuncName(
+        substraitExprName,
+        original.children.map(_.dataType),
+        ConverterUtils.FunctionConfig.OPT))
+
+    val expressionNodes = Lists.newArrayList(childNode)
+    val typeNode = ConverterUtils.getTypeNode(original.dataType, original.nullable)
+    ExpressionBuilder.makeScalarFunction(functionId, expressionNodes, typeNode)
+  }
+}
+
 case class CustomerExpressionTransformer() extends ExpressionExtensionTrait {
 
   /** Generate the extension expressions list, format: Sig[XXXExpression]("XXXExpressionName") */
   def expressionSigList: Seq[Sig] = Seq(
-    Sig[FloorDateTime]("floor_datetime")
+    Sig[FloorDateTime]("floor_datetime"),
+    Sig[PreciseCardinality]("ke_bitmap_cardinality"),
+    Sig[PreciseCountDistinctDecode]("ke_bitmap_cardinality"),
+    Sig[ReusePreciseCountDistinct]("ke_bitmap_or_data"),
+    Sig[PreciseCountDistinctAndValue]("ke_bitmap_and_value"),
+    Sig[PreciseCountDistinctAndArray]("ke_bitmap_and_ids"),
+    Sig[PreciseCountDistinct]("ke_bitmap_or_cardinality")
   )
 
   /** Replace extension expression to transformer. */
@@ -124,8 +157,81 @@ case class CustomerExpressionTransformer() extends ExpressionExtensionTrait {
         floorDateTime.timeZoneId,
         floorDateTime
       )
+    case preciseCardinality: PreciseCardinality =>
+      new KeBitmapFunctionTransformer(
+        substraitExprName,
+        ExpressionConverter
+          .replaceWithExpressionTransformer(preciseCardinality.child, attributeSeq),
+        preciseCardinality
+      )
+    case preciseCountDistinctDecode: PreciseCountDistinctDecode =>
+      new KeBitmapFunctionTransformer(
+        substraitExprName,
+        ExpressionConverter
+          .replaceWithExpressionTransformer(preciseCountDistinctDecode.child, attributeSeq),
+        preciseCountDistinctDecode
+      )
     case other =>
       throw new UnsupportedOperationException(
         s"${expr.getClass} or $expr is not currently supported.")
+  }
+
+  override def getAttrsIndexForExtensionAggregateExpr(
+      aggregateFunc: AggregateFunction,
+      mode: AggregateMode,
+      exp: AggregateExpression,
+      aggregateAttributeList: Seq[Attribute],
+      aggregateAttr: ListBuffer[Attribute],
+      resIndex: Int): Int = {
+    var reIndex = resIndex
+    aggregateFunc match {
+      case bitmap
+        if bitmap.getClass.getSimpleName.equals("ReusePreciseCountDistinct") ||
+          bitmap.getClass.getSimpleName.equals("PreciseCountDistinctAndValue") ||
+          bitmap.getClass.getSimpleName.equals("PreciseCountDistinctAndArray") ||
+          bitmap.getClass.getSimpleName.equals("PreciseCountDistinct") =>
+        mode match {
+          case Partial =>
+            val bitmapFunc = aggregateFunc
+              .asInstanceOf[TypedImperativeAggregate[Roaring64NavigableMap]]
+            val aggBufferAttr = bitmapFunc.inputAggBufferAttributes
+            for (index <- aggBufferAttr.indices) {
+              val attr = ConverterUtils.getAttrFromExpr(aggBufferAttr(index))
+              aggregateAttr += attr
+            }
+            reIndex += aggBufferAttr.size
+            reIndex
+          case Final =>
+            aggregateAttr += aggregateAttributeList(resIndex)
+            reIndex += 1
+            reIndex
+          case other =>
+            throw new UnsupportedOperationException(s"Unsupported aggregate mode: $other.")
+        }
+    }
+  }
+
+  override def buildCustomAggregateFunction(
+      aggregateFunc: AggregateFunction): (Option[String], Seq[DataType]) = {
+    val substraitAggFuncName = aggregateFunc match {
+      case countDistinct: PreciseCountDistinct =>
+        countDistinct.dataType match {
+          case LongType =>
+            Some("ke_bitmap_or_cardinality")
+          case BinaryType =>
+            Some("ke_bitmap_or_data")
+          case _ =>
+            throw new UnsupportedOperationException(
+              s"Aggregate function ${aggregateFunc.getClass} does not support the data type " +
+                s"${countDistinct.dataType}.")
+        }
+      case _ =>
+        extensionExpressionsMapping.get(aggregateFunc.getClass)
+    }
+    if (substraitAggFuncName.isEmpty) {
+      throw new UnsupportedOperationException(
+        s"Aggregate function ${aggregateFunc.getClass} is not supported.")
+    }
+    (substraitAggFuncName, aggregateFunc.children.map(child => child.dataType))
   }
 }
