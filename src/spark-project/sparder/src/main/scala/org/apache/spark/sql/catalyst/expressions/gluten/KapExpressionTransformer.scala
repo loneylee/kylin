e36287ddb7a65d71f71349ff562b768f45eb635c
jiawei.li <1019037687@qq.com>
2023-06-21 17:00:41 +0800

    support native floor_datetime function

---
+++ src/spark-project/sparder/src/main/scala/org/apache/spark/sql/catalyst/expressions/gluten/KapExpressionTransformer.scala
@@ -0,0 +1,131 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.spark.sql.catalyst.expressions.gluten
+
+import io.glutenproject.expression._
+import io.glutenproject.extension.ExpressionExtensionTrait
+import io.glutenproject.substrait.expression.{ExpressionBuilder, ExpressionNode}
+
+import org.apache.spark.internal.Logging
+import org.apache.spark.sql.catalyst.expressions._
+import io.glutenproject.backendsapi.clickhouse.CHBackendSettings
+import org.apache.spark.sql.internal.SQLConf
+import org.apache.spark.sql.types.StringType
+import org.apache.spark.unsafe.types.UTF8String
+
+import java.util.Locale
+
+class FloorDateTimeExpressionTransformer(
+  substraitExprName: String,
+  format: ExpressionTransformer,
+  timestamp: ExpressionTransformer,
+  timeZoneId: Option[String] = None,
+  original: FloorDateTime) extends ExpressionTransformer with Logging {
+
+  override def doTransform(args: java.lang.Object): ExpressionNode = {
+    // The format must be constant string in the fucntion date_trunc of ch.
+    if (!original.format.foldable) {
+      throw new UnsupportedOperationException(
+        s"The format ${original.format} must be constant string.")
+    }
+
+    val formatStr = original.format.eval().asInstanceOf[UTF8String]
+    if (formatStr == null) {
+      throw new UnsupportedOperationException("The format is null.")
+    }
+
+    val (newFormatStr, timeZoneIgnore) = formatStr.toString.toLowerCase(Locale.ROOT) match {
+      case "second" => ("second", false)
+      case "minute" => ("minute", false)
+      case "hour" => ("hour", false)
+      case "day" | "dd" => ("day", false)
+      case "week" => ("week", true)
+      case "mon" | "month" | "mm" => ("month", true)
+      case "quarter" => ("quarter", true)
+      case "year" | "yyyy" | "yy" => ("year", true)
+      // Can not support now.
+      // case "microsecond" => "microsecond"
+      // case "millisecond" => "millisecond"
+      case _ => throw new UnsupportedOperationException(s"The format $formatStr is invalidate.")
+    }
+
+    // Currently, data_trunc function can not support to set the specified timezone,
+    // which is different with session_time_zone.
+    if (
+      timeZoneIgnore && timeZoneId.nonEmpty &&
+        !timeZoneId.get.equalsIgnoreCase(
+          SQLConf.get.getConfString(
+            s"${CHBackendSettings.getBackendConfigPrefix()}.runtime_config.timezone")
+        )
+    ) {
+      throw new UnsupportedOperationException(
+        s"It doesn't support trunc the format $newFormatStr with the specified timezone " +
+          s"${timeZoneId.get}.")
+    }
+
+    val timestampNode = timestamp.doTransform(args)
+    val lowerFormatNode = ExpressionBuilder.makeStringLiteral(newFormatStr)
+
+    val functionMap = args.asInstanceOf[java.util.HashMap[String, java.lang.Long]]
+
+    val dataTypes = if (timeZoneId.nonEmpty) {
+      Seq(original.format.dataType, original.timestamp.dataType, StringType)
+    } else {
+      Seq(original.format.dataType, original.timestamp.dataType)
+    }
+
+    val functionId = ExpressionBuilder.newScalarFunction(
+      functionMap,
+      ConverterUtils.makeFuncName(substraitExprName, dataTypes))
+
+    val expressionNodes = new java.util.ArrayList[ExpressionNode]()
+    expressionNodes.add(lowerFormatNode)
+    expressionNodes.add(timestampNode)
+    if (timeZoneId != None) {
+      expressionNodes.add(ExpressionBuilder.makeStringLiteral(timeZoneId.get))
+    }
+
+    val typeNode = ConverterUtils.getTypeNode(original.dataType, original.nullable)
+    ExpressionBuilder.makeScalarFunction(functionId, expressionNodes, typeNode)
+  }
+}
+
+case class CustomerExpressionTransformer() extends ExpressionExtensionTrait {
+
+  /** Generate the extension expressions list, format: Sig[XXXExpression]("XXXExpressionName") */
+  def expressionSigList: Seq[Sig] = Seq(
+    Sig[FloorDateTime]("floor_datetime")
+  )
+
+  /** Replace extension expression to transformer. */
+  override def replaceWithExtensionExpressionTransformer(
+    substraitExprName: String,
+    expr: Expression,
+    attributeSeq: Seq[Attribute]): ExpressionTransformer = expr match {
+    case floorDateTime: FloorDateTime =>
+      new FloorDateTimeExpressionTransformer(
+        substraitExprName,
+        ExpressionConverter.replaceWithExpressionTransformer(floorDateTime.format, attributeSeq),
+        ExpressionConverter.replaceWithExpressionTransformer(floorDateTime.timestamp, attributeSeq),
+        floorDateTime.timeZoneId,
+        floorDateTime
+      )
+    case other =>
+      throw new UnsupportedOperationException(
+        s"${expr.getClass} or $expr is not currently supported.")
+  }
+}
